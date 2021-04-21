1ba36b95f64b6a7b118886632cebf77e0cd4ba43
JP <jp.touron@gmail.com>
2021-04-22 00:16:44 +0200

    Step 3 completed. created Game
    On branch take1
    new file:   src/Zombies.Domain.Tests/GameShould.cs
    new file:   src/Zombies.Domain/Game.cs

---
+++ src/Zombies.Domain.Tests/GameShould.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Xunit;
+
+namespace Zombies.Domain.Tests
+{
+    public class GameShould
+    {
+        [Fact]
+        public void BeAbleToAddANewSurivor()
+        {
+            var sut = new Game();
+            var survivor = Utils.CreateSurvivor();
+
+            sut.AddSurvivor(survivor);
+
+            Assert.Equal(1, sut.SurvivorCount);
+        }
+
+        [Fact]
+        public void GameEndsWhenAllSuvivorsDie()
+        {
+            var sut = new Game();
+            var survivor1 = Utils.CreateSurvivor();
+            var survivor2 = Utils.CreateSurvivor();
+            var survivor3 = Utils.CreateSurvivor();
+            sut.AddSurvivor(survivor1);
+            sut.AddSurvivor(survivor2);
+            sut.AddSurvivor(survivor3);
+
+            survivor1.Wound(3);
+            survivor2.Wound(3);
+            survivor3.Wound(3);
+
+
+            Assert.Equal( Game.GamesState.Finished, sut.State);
+        }
+        [Fact]
+        public void GameIsOnGoingIfAnySurvivorsLive()
+        {
+            var sut = new Game();
+            var survivor1 = Utils.CreateSurvivor();
+            var survivor2 = Utils.CreateSurvivor();
+            var survivor3 = Utils.CreateSurvivor();
+            sut.AddSurvivor(survivor1);
+            sut.AddSurvivor(survivor2);
+            sut.AddSurvivor(survivor3);
+
+            survivor1.Wound(3);
+            survivor2.Wound(3);
+
+
+            Assert.Equal(Game.GamesState.OnGoing, sut.State);
+        }
+
+        [Fact]
+        public void ThrowWhenAddingANullSurvivor()
+        {
+            var sut = new Game();
+            Survivor survivor = null;
+
+            Assert.Throws<ArgumentNullException>(() => sut.AddSurvivor(survivor));
+        }
+
+        [Fact]
+        public void ThrowWhenAddingASurvivorWithExistingName()
+        {
+            var sut = new Game();
+
+            var survivor = Utils.CreateSurvivor();
+            sut.AddSurvivor(survivor);
+
+            survivor = Utils.CreateSurvivor();
+            sut.AddSurvivor(survivor);
+
+            Assert.Throws<InvalidOperationException>(() => sut.AddSurvivor(survivor));
+        }
+
+        public class BeCreated
+        {
+            [Fact]
+            public void WithZeroSurvivors()
+            {
+                var sut = new Game();
+
+                Assert.Equal(0, sut.SurvivorCount);
+                Assert.Equal(Game.GamesState.Finished, sut.State);
+            }
+        }
+    }
+}
---
+++ src/Zombies.Domain/Game.cs
@@ -0,0 +1,52 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zombies.Domain
+{
+    public class Game
+    {
+
+        public enum GamesState
+        {
+
+            OnGoing,
+            Finished
+        }
+
+        public GamesState State
+        {
+            get
+            {
+
+                if (survivors.Any(x => x.CurrentState == IHealth.State.Alive))
+                    return GamesState.OnGoing;
+                else
+                    return GamesState.Finished;
+            }
+        }
+
+        private IList<Survivor> survivors;
+
+        public Game()
+        {
+            survivors = new List<Survivor>();
+        }
+
+        public int SurvivorCount => survivors.Count;
+
+        public void AddSurvivor(Survivor survivor)
+        {
+            Guard.Against.Null(survivor, nameof(survivor));
+
+            if (survivors.Contains(survivor))
+                throw new InvalidOperationException($"A survivor with the name {survivor.Name} already exists.");
+
+            survivors.Add(survivor);
+        }
+
+
+
+    }
+}
