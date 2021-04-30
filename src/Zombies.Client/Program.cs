014af46401c58baf1a1316c1235b7230c7c49bc3
JP <jp.touron@gmail.com>
2021-04-30 21:06:16 +0200

    quick console client implementation
    On branch take1
    modified:   src/Zombies.Client/Program.cs
    modified:   src/Zombies.sln

--- src/Zombies.Client/Program.cs
+++ src/Zombies.Client/Program.cs
@@ -6,29 +6,36 @@ using Zombies.Domain;
 
 namespace Zombies.Client
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
             //JP: todos los accesibility modifiers estan bien, desde aca no puedo CREAR nada de domain, solo leer
-            /*lo que falta son: 
+            /*lo que falta son:
              * tests para todo lo q es history
              * definir bien las interfaces internal y public para mantener encapsulamiento
              * actualizar los tests q fallen por accesibilidad
              * definir bien los factories (ya estan practicamente hechos)
              * no mucho mas
-             
-            
-             
+
              */
-            var g = new GameProvider().CreateGame();
+            var g = Providers.Game();
 
-            var s = new Application.SurvivorProvider().CreateSurvivor("Juan");
-            g.AddSurvivor(s);
+            //JP: MAKE A TEST THAT WE CANNOT ADD AN ISURVIVOR TO THE GAME, JUST A ISURVIVORHISTORY
+            var s = g.AddSurvivor("Juan");
+
+            var s2 = g.AddSurvivor("Pedro");
 
             s.Wound(1);
+            s2.Kill(new Zombie());
+            var golfClub = Providers.Equipment("golf club");
+            s.AddEquipment(golfClub);
+            s.RightHandEquip = golfClub;
+            for (int i = 0; i < 6; i++)
+                s.Kill(new Zombie());
             s.Wound(1);
+            s2.Kill(new Zombie());
+            s2.Wound(4);
 
             var sb = new StringBuilder();
             g.Events.ToList().ForEach(x => sb.AppendLine(x));
